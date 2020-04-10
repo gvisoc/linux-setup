42f64cb7a2a37764a9916df29a6c4d0bf1964301
Gabriel Viso Carrera <4035415+gvisoc@users.noreply.github.com>
2020-04-10 11:13:42 +1000

    Fix packages

--- ubuntu/user-misc.sh
+++ ubuntu/user-misc.sh
@@ -10,7 +10,7 @@ apt -y install firefox
 
 # Backup utility a la Time Machine with support for NFS Mounts 
 # and libraries for setting up a Local Encrypted configuration
-apt -y install backintime backintime-gnome nfs-common encfs
+apt -y install backintime-common backintime-gnome nfs-common encfs
 
 # Command line utility to interact with the clipboards
 apt -y install xclip xsel
