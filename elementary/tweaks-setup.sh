b625f8f61796a8c7dbf5769ebc06ade79db347fa
Gabriel Viso Carrera <REDACTED>
2020-04-22 16:34:33 +1000

    Some refactoring here and there

--- elementary/tweaks-setup.sh
+++ elementary/tweaks-setup.sh
@@ -13,17 +13,3 @@ apt update
 # Enable dark theme / theming in elementary OS
 apt -y install software-properties-common
 apt -y install elementary-tweaks
-
-# Old indicators panel for elementary
-# For Telegram, Dropbox, Bitwarden,... tray icons
-ORIG_DIR=`pwd`
-mkdir -p ~/.setup-tmp
-cd ~/.setup-tmp
-curl --remote-name http://ppa.launchpad.net/elementary-os/stable/ubuntu/pool/main/w/wingpanel-indicator-ayatana/wingpanel-indicator-ayatana_2.0.3+r27+pkg17~ubuntu0.4.1.1_amd64.deb
-dpkg -i wingpanel-indicator-ayatana_2.0.3+r27+pkg17\~ubuntu0.4.1.1_amd64.deb
-if [ $? -eq 0 ]; then
-    # remove duplicated network indicator from old panel
-    sudo mv /etc/xdg/autostart/nm-applet.desktop /etc/xdg/autostart/nm-applet.desktop.old
-fi
-cd $ORIG_DIR
-rm -rf ~/.setup-tmp 
