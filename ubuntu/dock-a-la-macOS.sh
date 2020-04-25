c769f72e15e53acf10ccb47851d7f0234603a915
Gabriel Viso Carrera <4035415+gvisoc@users.noreply.github.com>
2020-04-25 20:19:24 +1000

    Dock setup

---
+++ ubuntu/dock-a-la-macOS.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ $EUID != 0 ]; then
+    sudo "$0" "$@"
+    exit $?
+fi
+
+apt update
+apt -y install dconf-editor
+
+gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
+gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode FIXED
+gsettings set org.gnome.shell.extensions.dash-to-dock unity-backlit-items true
