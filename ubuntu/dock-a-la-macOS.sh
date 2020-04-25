5365bbee43e77d2aef9761edaa393549a3bef715
Gabriel Viso Carrera <4035415+gvisoc@users.noreply.github.com>
2020-04-25 22:02:32 +1000

    Dock to autohide on focused window overlap

--- ubuntu/dock-a-la-macOS.sh
+++ ubuntu/dock-a-la-macOS.sh
@@ -11,3 +11,5 @@ gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
 gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
 gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode FIXED
 gsettings set org.gnome.shell.extensions.dash-to-dock unity-backlit-items true
+gsettings set org.gnome.shell.extensions.dash-to-dock intellihide true
+gsettings set org.gnome.shell.extensions.dash-to-dock intellihide-mode 'FOCUS_APPLICATION_WINDOWS'
